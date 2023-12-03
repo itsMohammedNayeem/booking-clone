49ce322fa732499c8ac757b2ca29439f9b0d15c9
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-03 18:03:07 +0530

    Implemented Oxylabs scraping

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,28 @@ export default function Home() {
   return (
     <main className='bg-darkPowderBlue'>
       <section className='mx-auto max-w-7xl p-6'>
-        <h2 className='text-5xl font-bold text-white'>Find your next stay</h2>
-        <h3 className='py-5 text-xl text-white'>Search low prices on hotels, homes, flights, and much more...</h3>
+        <h2 className='text-5xl font-bold text-white'>Find your Next Stay</h2>
+        <h3 className='py-5 text-xl text-white'>Search low prices on hotels, homes and much more...</h3>
       </section>
 
       <section className='m-4 -mb-14 mt-0 px-2 lg:px-4'>
         <SearchForm />
       </section>
+
       <section className='mx-auto mt-10 max-w-7xl rounded-t-lg bg-white p-6'>
         <div className='pt-5'>
-          <h3 className='text-xl font-bold'>Trending destinations</h3>
-          <p className='font-light'>Popular destinations for your next trip</p>
+          <h3 className='text-xl font-bold'>Trending Destinations</h3>
+          <p className='font-light'>Most popular choices for travellers from around the world</p>
         </div>
 
         <div className='flex space-x-4 overflow-x-scroll py-5'>
           {trending_data.map(item => (
             <div key={item.id} className='shrink-0 cursor-pointer space-y-1'>
-              <img key={item.id} src={item.src} alt={item.title} className='h-72 w-80 rounded-lg object-cover pb-2' />
-              <p className='text-sm font-bold'>{item.title}</p>
+              <img key={item.id} className='h-72 w-80 rounded-lg object-cover pb-2' src={item.src} alt='' />
+
+              <p className='font-bold'>{item.title}</p>
+              <p className=''>{item.location}</p>
+              <p className='text-sm font-light'>{item.description}</p>
             </div>
           ))}
         </div>
